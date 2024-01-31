f329593bc163c3a2e5dab00e41c1a308e45972ca
Matt jones <matt@jones.com>
2024-01-31 16:34:36 -0500

    edited app

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { Route, Routes } from 'react-router-dom'
 import { Characters } from './Characters';
 import { Character } from './Character';
 import { Films } from './Films';
+import { Planets } from './Planets';
 import './App.css'
 
 export function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<><Characters /><Films /></>}></Route>
+        <Route path="/" element={<><Characters /><Films />
+        <Planets /></>}></Route>
         <Route path="/characters/:id" element={<Character />}></Route>
       </Routes>
     </>
