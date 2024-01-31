25735cb648378e0a22b85a112382e539ac099077
Ben Cat <ben@cat.com>
2024-01-31 11:56:12 -0500

    added some routes

--- server.js
+++ server.js
@@ -1,21 +1,116 @@
-// The web server
 import express from 'express';
-// import { MongoClient } from 'mongodb';
+import { MongoClient } from 'mongodb';
 
 const app = express();
 
 app.use(express.json());
 
-//Serve all the planets at GET /planets
+app.get("/api/characters", async (req, res) => {
+  try {
+    const client = await MongoClient.connect('mongodb://localhost:27017');
+    const db = client.db('swapi');
+    const collection = db.collection('characters');
+    const characters = await collection.find().toArray();
+    client.close();
+    res.json(characters);
+  } catch (error) {
+    res.status(500).json({error: error});
+  }
+});
+
+app.get("/api/films", async (req, res) => {
+  try {
+    const client = await MongoClient.connect('mongodb://localhost:27017');
+    const db = client.db('swapi');
+    const collection = db.collection('films');
+    const films = await collection.find().toArray();
+    client.close();
+    res.json(films);
+  } catch (error) {
+    res.status(500).json({error: error});
+  }
+});
+
 app.get("/api/planets", async (req, res) => {
   try {
-    // const client = await MongoClient.connect('mongodb://localhost:27017');
-    // const db = client.db('swapi');
-    // const collection = db.collection('planets');
-    // const planets = await collection.find().toArray();
-    // client.close();
-    const planets = {"fake":"object"}
+    const client = await MongoClient.connect('mongodb://localhost:27017');
+    const db = client.db('swapi');
+    const collection = db.collection('planets');
+    const planets = await collection.find().toArray();
+    client.close();
     res.json(planets);
+  } catch (error) {
+    res.status(500).json({error: error});
+  }
+});
+
+app.get("/api/characters/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const client = await MongoClient.connect('mongodb://localhost:27017');
+    const db = client.db('swapi');
+    const collection = db.collection('characters');
+    const character = await collection.findOne({'id': +id});
+    client.close();
+    if(character) {
+      res.json(character);
+    } else {
+      res.status(404).json({message: 'Character not found'});
+    }
+  } catch (error) {
+    res.status(500).json({error: error});
+  }
+});
+
+app.get("/api/films/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const client = await MongoClient.connect('mongodb://localhost:27017');
+    const db = client.db('swapi');
+    const collection = db.collection('films');
+    const film = await collection.findOne({'id': +id});
+    client.close();
+    if(film) {
+      res.json(film);
+    } else {
+      res.status(404).json({message: 'Film not found'});
+    }
+  } catch (error) {
+    res.status(500).json({error: error});
+  }
+});
+
+app.get("/api/planets/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const client = await MongoClient.connect('mongodb://localhost:27017');
+    const db = client.db('swapi');
+    const collection = db.collection('planets');
+    const planet = await collection.findOne({'id': +id});
+    client.close();
+    if(planet) {
+      res.json(planet);
+    } else {
+      res.status(404).json({message: 'Planet not found'});
+    }
+  } catch (error) {
+    res.status(500).json({error: error});
+  }
+});
+
+app.get("/api/films/:id/characters", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const client = await MongoClient.connect('mongodb://localhost:27017');
+    const db = client.db('swapi');
+    const collection = db.collection('films_characters');
+    const characters = await collection.find({'film_id': +id}).toArray();
+    client.close();
+    if(characters) {
+      res.json(characters);
+    } else {
+      res.status(404).json({message: 'Character not found'});
+    }
   } catch (error) {
     res.status(500).json({error: error});
   }
