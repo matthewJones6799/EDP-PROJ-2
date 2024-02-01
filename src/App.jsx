fde3cc94991a791c7d9dc9d97e94cfdef6478bb5
Ben Cat <ben@cat.com>
2024-02-01 10:03:40 -0500

    Removed films

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { Route, Routes } from 'react-router-dom'
 import { Characters } from './Characters';
 import { Character } from './Character';
 import { Film } from './Film';
-import { Films } from './Films';
 import { Planet } from './Planet';
 import { Planets } from './Planets';
 import './App.css'
@@ -11,7 +10,7 @@ export function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<><Characters /><Films /><Planets /></>}></Route>
+        <Route path="/" element={<><Characters /><Planets /></>}></Route>
         <Route path="/characters/:id" element={<Character />}></Route>
 
         <Route path="/films/:id" element = {<Film />}></Route>
